80560fa3a44abd4cbbd11916f0efe312171ab618
Vishakha <vishakha@coursera.com>
2017-10-21 10:23:05 -0700

    this is final commit

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -4,9 +4,19 @@
     .controller('LunchCheckController', LunchCheckController);
     LunchCheckController.$inject = ['$scope'];
     function LunchCheckController($scope) {
+
         $scope.lunchItemsString = '';
+        $scope.lunchInfo = '';
+
         $scope.checkIfTooMuch = function(){
-            console.log($scope.lunchItemsString);
+            var items = $scope.lunchItemsString.split(",");
+            if(items.length == 1 && items[0] == ""){ // empty check
+                $scope.lunchInfo = 'Please enter data first';
+            } else if(items.length <= 3) { // less than or equal to 3 food items
+                $scope.lunchInfo = 'Enjoy !';
+            } else { // more that 3 food items
+                $scope.lunchInfo = 'Too much !';
+            }
         }
     }
 })();
