4064fc1cf6aa49e853a0ddc2690aa16f835fa7e5
Vishakha Vaidywan <vishakhavaidywan@gmail.com>
2018-04-15 17:46:12 +0530

    app.js modified

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,18 +1,19 @@
 (function () {
     'use strict';
     angular.module('LunchCheck', ['ngRoute'])
-    .controller('LunchCheckController', LunchCheckController);
+        .controller('LunchCheckController', LunchCheckController);
     LunchCheckController.$inject = ['$scope'];
     function LunchCheckController($scope) {
 
         $scope.lunchItemsString = '';
         $scope.lunchInfo = '';
 
-        $scope.checkIfTooMuch = function(){
+        //empty item in the list - case not handled (not required for grading)
+        $scope.checkIfTooMuch = function () {
             var items = $scope.lunchItemsString.split(",");
-            if(items.length == 1 && items[0] == ""){ // empty check
+            if (items.length == 1 && items[0] == "") { // empty check
                 $scope.lunchInfo = 'Please enter data first';
-            } else if(items.length <= 3) { // less than or equal to 3 food items
+            } else if (items.length <= 3) { // less than or equal to 3 food items
                 $scope.lunchInfo = 'Enjoy !';
             } else { // more that 3 food items
                 $scope.lunchInfo = 'Too much !';
